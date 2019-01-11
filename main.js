29b1d1bc8b04c7fca7de331877eca5d65393b242
Anastasiya Stanevich <Anastasiya.Stanevich@itechart-group.com>
2019-01-11 14:58:38 +0300

    The End

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 const searchBtn = document.querySelector("#search");
-const overlay = document.querySelector(".overlay");
+const overlay   = document.querySelector(".overlay");
+const closeBtn  = document.querySelector("#close-btn");
+const navBtn    = document.querySelector("#menu");
+let navIsClosed = true;
 
-searchBtn.addEventListener("click", function(){
-    overlay.classList.remove("hidden");
-})
-
-const navBtn = document.querySelector("#menu");
-let navIsClose = true;
-
-navBtn.addEventListener("click", function(){
+navBtn.addEventListener("click", function() {
     const responsiveNav = document.querySelector("#responsive-nav");
-    if (navIsClose) {
-        responsiveNav.classList.remove("hidden");
-        navIsClose = false;
+
+    if (navIsClosed) {
+        responsiveNav.classList.remove("closed-nav");
+        navIsClosed = false;
     } else {
-        responsiveNav.classList.add("hidden");
-        navIsClose = true;
+        responsiveNav.classList.add("closed-nav");
+        navIsClosed = true;
     };
 });
 
-const closeBtn = document.querySelector("#close-btn");
-closeBtn.addEventListener("click", function(){
-    overlay.classList.add("hidden");
+searchBtn.addEventListener("click", function() {
+    overlay.classList.remove("closed-overlay");   
+});
+closeBtn.addEventListener("click", function() {
+    overlay.classList.add("closed-overlay");
 })
